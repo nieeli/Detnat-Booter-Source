6ece53a6e2807c4e7dc837d9ebf2bcb2ca945efe
Haseo Fair <callmeguyfawks@gmail.com>
2013-05-21 23:21:47 -0400

    Terms of service added for Law protection

---
+++ tos.php
@@ -0,0 +1,58 @@
+<html>
+<body>    
+<!-- Main content wrapper -->
+    <div class="wrapper">
+<div class="title"><img src="images/icons/dark/list.png" alt="" class="titleIcon" /><h3><strong><font color=red>* By using our stresser you agree to our Terms Of Service</h2></div>
+<div class="formRow">
+<label>
+	<font color="black"><strong>
+<li><br>-1. Terms
+
+By accessing this web site, you are agreeing to be bound by these web site Terms and Conditions of Use, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this web site are protected by applicable copyright and trade mark law.</li>
+<li><br>-2. Use License
+
+Permission is granted to stress test dedicated servers and networks owned by you. This is the oppurtunity to make your firewalls better, not to misuse against the law. This is a license given to you and anything you do while on http://dotbooter.com is your own responsibility as we are not liable. Under this license you may not:
+Modify or attempt to download any source code connected tos (sitename.com);
+Intentionally send a ddos flood to an IP address not owned by yourself;
+spam any servers connected with (sitename.com) or related hosts;
+remove any copyright or other proprietary notations from the materials; or
+transfer the materials to another person "such as selling the account" or "mirror" the materials on any other server;
+own more than one account per person;
+user our product for personal gain and/or to profit;
+request refunds or attempt to dispute any payments to our service.
+You are liable for what you do at(sitename.com), if you break any of these terms then you may be banned without any refunds and subjected to U.S Laws if necessary.</li>
+<li><br>-3. Disclaimer
+
+The materials on the (sitename.com) web site are provided "as is".(sitename.com) makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights. Further, (sitename.com) does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its Internet web site or otherwise relating to such materials or on any sites linked to this site.</li>
+<li><br>-4. Limitations
+
+In no event shall (sitename.com) or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption,) arising out of the use or inability to use the materials on (sitename.com) Internet site, even if an authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.</li>
+<li><br>-5. Revisions and Errata
+
+The materials appearing on (sitename.com) could include technical, typographical, or photographic errors.(sitename.com) does not warrant that any of the materials on its web site are accurate, complete, or current. (sitename.com) may make changes to the materials contained on its web site at any time without notice. (sitename.com) does not, however, make any commitment to update the materials.</li>
+<li><br>-6. Site Terms of Use Modifications
+
+(sitename.com) may revise these terms of use for its web site at any time without notice. By using this web site you are agreeing to be bound by the then current version of these Terms and Conditions of Use.</li>
+<li><br>-7. Governing Law
+
+Any claim relating to the (sitename.com) web site shall be governed by the laws of the State of Michigan without regard to its conflict of law provisions.
+
+General Terms and Conditions applicable to Use of a Web Site.
+
+Privacy Policy
+
+Your privacy is very important to us. Accordingly, we have developed this Policy in order for you to understand how we collect, use, communicate and disclose and make use of personal information. The following outlines our privacy policy.
+
+Before or at the time of collecting personal information, we will identify the purposes for which information is being collected.
+We will collect and use of personal information solely with the objective of fulfilling those purposes specified by us and for other compatible purposes, unless we obtain the consent of the individual concerned or as required by law.
+We will only retain personal information as long as necessary for the fulfillment of those purposes.
+We will collect personal information by lawful and fair means and, where appropriate, with the knowledge or consent of the individual concerned.
+Personal data should be relevant to the purposes for which it is to be used, and, to the extent necessary for those purposes, should be accurate, complete, and up-to-date.
+We will protect personal information by reasonable security safeguards against loss or theft, as well as unauthorized access, disclosure, copying, use or modification.
+We will make readily available to customers information about our policies and practices relating to the management of personal information.
+We are committed to conducting our business in accordance with these principles in order to ensure that the confidentiality of personal information is protected and maintained.</li>
+</label>
+</div>
+	</body>
+
+</html>
