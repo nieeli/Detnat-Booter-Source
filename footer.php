89491a70decc7d81471638dfa0ce01be45f8e92f
Haseo Fair <callmeguyfawks@gmail.com>
2013-05-21 23:21:32 -0400

    Footer added ($sitename with the Terms of service Link)

---
+++ footer.php
@@ -0,0 +1,12 @@
+<head>   <link rel="shortcut icon" href="favicon.ico" >
+<?php
+if (!defined('DIRECT'))
+{
+	die('Direct access not allowed.');
+}
+?>
+<div id="footer">
+
+    <div class="wrapper"><font color=Lime>Copyright &copy; <?php echo date('Y')?> Detnat <br><font color=cyan><sup class="pull-left" style="padding-top: 5px;">* By using our stresser you agree to our <a href="javascript:;" onClick="window.open('/boot/tos.php','myWin','scrollbars=yes,width=200,height=300');">Terms of Service</a></sup>
+</div>
+</div>
